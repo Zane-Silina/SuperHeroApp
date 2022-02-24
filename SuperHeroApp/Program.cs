fb5e95ba797b9053b29d213f8592c299f059f985
kasutaja <kasutaja@DESKTOP-2J3V6O9>
2022-02-24 20:48:25 +0200

    Added variables and data types.

--- SuperHeroApp/Program.cs
+++ SuperHeroApp/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string heroName = "Flow";
+int heroAge = 31;
+string power = "inteligence";
+string weakness = "cynicism";
+Console.WriteLine("------------------------");
+Console.WriteLine($"Hero Name: {heroName}");
+Console.WriteLine($"Age: {heroAge}");
+Console.WriteLine($"Super power: {power}");
+Console.WriteLine($"Weakness: {weakness}");
+
+Console.WriteLine("------------------------");
+string heroName2 = "Krit";
+int heroAge2 = 47;
+string power2 = "martial arts";
+string weakness2 = "impatience";
+
+Console.WriteLine($"Hero Name: {heroName2}");
+Console.WriteLine($"Age: {heroAge2}");
+Console.WriteLine($"Super power: {power2}");
+Console.WriteLine($"Weakness: {weakness2}");
+Console.WriteLine("------------------------");
